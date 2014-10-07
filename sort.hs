c0a5428c642e10d79e7bf85f56d922be22ce145e
Michał Kawalec <michal@bazzle.me>
2014-10-07 17:26:55 +0200

    more beauty

--- sort.hs
+++ sort.hs
@@ -1,3 +1,5 @@
 sort :: Ord a => [a] -> [a]
 sort []     = []
-sort (x:xs) = (sort [y | y <- xs, y < x]) ++ [x] ++ (sort [y | y <- xs, y >= x])
+sort (x:xs) = (sort [y | y <- xs, y < x]) ++ 
+              [x] ++ 
+              (sort [y | y <- xs, y >= x])
